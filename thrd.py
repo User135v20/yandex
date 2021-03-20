33bf0d85588fbd6b4eff234c6f16e9cef5dd5f24
roman <33gra@mail.ru>
2021-03-20 16:01:16 +0300

    zadacha ne reshena

--- thrd.py
+++ thrd.py
@@ -1,32 +1,48 @@
 def min_dist(list, index, k, n):
     difference = []
     min = 0
-    z = 0
-    for i in range(-k, k+1):
-        qwerty = index + i
+
+
+    # if index - k == 0:
+    #     start = 0
+    # elif index + k == n:
+    #     fin = n
+    # else:
+    #     start = k
+    #     fin = k
+
+    for i in range(-k, k + 1):
         if index + i < 0 or index + i >= n or i == 0:
             continue
         else:
-
             difference.append(abs(list[index + i] - list[index]))
-
     difference.sort()
-
     for i in range(k):
         min += int(difference[i])
     return min
 
 
-str = []
-str = input()
-str = list(map(int, str.split()))
-# print (type(str))
-n = str[0]
-k = str[1]
-str = input("введите массив: ")
-str = list(map(int, str.split()))
-mas = []
-mas = sorted(str)
-for i in range(n):
-    str[i] = min_dist(mas,i,k,n)
-print(*str)
+def main():
+    result = []
+    dict_result = {}
+    str = input()
+    str = list(map(int, str.split()))
+    n = str[0]
+    k = str[1]
+    str = input()
+    str = list(map(int, str.split()))
+    mas = sorted(str)
+
+    for i in range(n):
+        index = mas.index(str[i])
+        if str[i] in dict_result:
+            result.append(dict_result[str[i]])
+        else:
+            res = min_dist(mas, index, k, n)
+            result.append(res)
+            dict_result[str[i]] = res
+    print(*result)
+
+
+if __name__ == '__main__':
+    main()
